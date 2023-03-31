f000fb182bc21511d95e8c71cc69eb8810647254
09shivanshu <shivanshu09@gmail.com>
2023-03-31 22:19:02 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+def get_requirements()-> list(str):
+    with open (REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list =requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list 
+
+
+
+setup(
+    jls_extract_var = "sensor",
+    version = "0.0.1",
+    author = "ineuron",
+    author_email = "shivanshu09@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements(),
+)
